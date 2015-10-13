dfb914257d33aff9c65e2db70a78bdc3dc2a7d9f
rogermelich <rogermelich@iesebre.com>
2015-10-13 21:13:39 +0200

    modified main.php to version 2.0

--- main.php
+++ main.php
@@ -11,8 +11,9 @@ class Model
     public $string;
 
     public function __construct(){
-        $this->string = "MVC + PHP = Awesome!";
+        $this->string = "MVC + PHP = Awesome, click here!";
     }
+
 }
 
 class View
@@ -25,8 +26,8 @@ class View
         $this->model = $model;
     }
 
-    public function output(){
-        return "<p>" . $this->model->string . "</p>";
+    public function output() {
+        return '<p><a href="main.php?action=clicked">' . $this->model->string . "</a></p>";
     }
 }
 
@@ -34,12 +35,21 @@ class Controller
 {
     private $model;
 
-    public function __construct($model) {
+    public function __construct($model){
         $this->model = $model;
+    }
+
+    public function clicked() {
+        $this->model->string = "Updated Data, thanks to MVC and PHP!";
     }
 }
 
 $model = new Model();
 $controller = new Controller($model);
 $view = new View($controller, $model);
+
+if (isset($_GET['action']) && !empty($_GET['action'])) {
+    $controller->{$_GET['action']}();
+}
+
 echo $view->output();
